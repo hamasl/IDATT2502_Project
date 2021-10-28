baeab08abc0dc723caa5a56e778b6228f894bcd9
Martin Hagen <martin@partins.no>
2021-10-28 12:27:08 +0200

    Added unit testing, moved ommon strings to constant file

--- name_simplifier.py
+++ name_simplifier.py
@@ -1,4 +1,8 @@
+import simplifier_constants as sc
+
 """
+
+Converts all strings, chars and floats to generic token symboling their type. 
 Input: ["int", "a", "=", "33", ";", "a", "=", "35", ";", "int", "b", "=", "40", ";", "void",
             "parse", "(", "int", "c", ",", "int", "d", ")", ";",
             "parse", "(", "a", ",", "b", ")", ";"]
@@ -7,7 +11,8 @@ Output: ["int", "a", "=", "BASE_INT", ";", "a", "=", "BASE_INT", ";", "int", "b"
             "parse", "(", "a", ",", "b", ")", ";"]
 """
 
-dict = ["int", "*", ";", "(", ")", "=", "void", ",", "BASE_INT"]
+dict = ["+", "}", "{", "float", "char", "int", "*", ";", "(", ")", "=", "void", ",", sc.BASE_STRING, sc.BASE_FLOAT,
+        sc.BASE_CHAR]
 
 
 def convert_to_base_types(input):
@@ -15,11 +20,11 @@ def convert_to_base_types(input):
     for i in range(len(input)):
         if input[i] not in dict:
             if input[i].startswith('"') and input[i].endswith('"'):
-                output.append("BASE_STRING")
+                output.append(sc.BASE_STRING)
             elif input[i].startswith("'") and input[i].endswith("'"):
-                output.append("BASE_CHAR")
+                output.append(sc.BASE_CHAR)
             elif "." in input[i] and input[i].replace(".", "", 1).isdigit():
-                output.append("BASE_FLOAT")
+                output.append(sc.BASE_FLOAT)
             else:
                 output.append(input[i])
 
@@ -29,90 +34,29 @@ def convert_to_base_types(input):
 
 
 """
-Input: ["int", "a", "=", "BASE_INT", ";", "a", "=", "BASE_INT", ";", "int", "b", "=", "BASE_INT", ";", "void",
-            "parse", "(", "a", ",", "b", ")", ";"]
-Output: [
-    'FUNC0', '(', 'BASE_INT', ',', 'BASE_INT', ')', ';']
+Converts function names to FUNC. Variable names are changed to ID0, ID1 ... When the same variable name is hit it is give the same ID.
+Period length decides the number of iterations before the variable_names array is emptied and the we start again at ID0, ID1 ...
+This is done to be able to send multiple functions at the same time.
 """
 
 
-# int a = [10]; a[12] = input()
-# int a[BASE_INT]; a [BASE_INT]
-# int a  = 2; a += 2; printf(a)
-# printf(4)
 def simplify(input, period_length):
-    function_base_name = "FUNC"
-    variable_base_name = "ID"
     variable_names = []
     output = []
-    # TODO dict also needs to contain BASE tokens for strings and integers
-    # TODO get dict from same source as lexer
-
     for i in range(len(input)):
         if i % period_length == 0:
             variable_names = []
         if input[i] not in dict:
-            if len(input) >= i and input[i + 1] == "(":
-                output.append(function_base_name)
+            if len(input) > i + 1 and input[i + 1] == "(":
+                output.append(sc.GENERIC_FUNCTION_NAME)
             elif not input[i].isdigit():
                 if input[i] in variable_names:
-                    output.append(variable_base_name + str(variable_names.index(input[i])))
+                    output.append(sc.GENERIC_VARIABLE_BASE_NAME + str(variable_names.index(input[i])))
                 else:
-                    output.append(variable_base_name + str(len(variable_names)))
+                    output.append(sc.GENERIC_VARIABLE_BASE_NAME + str(len(variable_names)))
                     variable_names.append(input[i])
             else:
                 output.extend(list(input[i]))
         else:
             output.append(input[i])
     return output
-
-"""
-Input: ["int", "a", "=", "BASE_INT", ";", "a", "=", "BASE_INT", ";", "int", "b", "=", "BASE_INT", ";", "void",
-            "parse", "(", "int", "c", ",", "int", "d", ")", ";",
-            "parse", "(", "a", ",", "b", ")", ";"]
-Output: ['int', 'ID0', '=', 'BASE_INT', ';', 'ID0', '=', 'BASE_INT', ';', 'int', 'ID1', '=', 'BASE_INT', ';',
-            'void', 'FUNC0', '(', 'int', 'ID0', ',', 'int', 'ID1', ')', ';',
-            'FUNC0', '(', 'ID0', ',', 'ID1', ')', ';']
-"""
-
-
-def simplify_old(input, period_length):
-    variable_base_name = "ID"
-    function_base_name = "FUNC"
-    variable_names = []
-    function_names = []
-    output = []
-    # TODO dict also needs to contain BASE tokens for strings and integers
-    # TODO get dict from same source as lexer
-
-    for i in range(len(input)):
-        if i % period_length == 0:
-            variable_names = []
-            function_names = []
-        if input[i] not in dict:
-            if len(input) >= i and input[i + 1] == "(":
-                if input[i] in function_names:
-                    output.append(function_base_name + str(function_names.index(input[i])))
-                else:
-                    output.append(function_base_name + str(len(function_names)))
-                    function_names.append(input[i])
-            else:
-                if input[i] in variable_names:
-                    output.append(variable_base_name + str(variable_names.index(input[i])))
-                else:
-                    output.append(variable_base_name + str(len(variable_names)))
-                    variable_names.append(input[i])
-        else:
-            output.append(input[i])
-    return output
-
-
-if __name__ == '__main__':
-    test1 = ["int", "a", "=", "33", ";", "a", "=", "35", ";", "int", "b", "=", "40", ";",
-            "parse", "(", "a", ",", "b", ")", ";"]
-    print(test1)
-    print(simplify(convert_to_base_types(test1),150))
-
-    test = ["int", "a", "=", "BASE_INT", ";", "a", "=", "BASE_INT", ";", "int", "b", "=", "BASE_INT", ";",
-            "parse", "(", "a", ",", "b", ")", ";"]
-    #print(simplify(test, 150))
