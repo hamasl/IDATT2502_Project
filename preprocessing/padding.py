7b633daf186572dd326c0a28b6b87681bf278a2c
Diderik Kramer <diderikk@stud.ntnu.no>
2021-11-03 14:58:52 +0100

    Add padding and masking

---
+++ preprocessing/padding.py
@@ -0,0 +1,15 @@
+import torch
+
+def masking_padding(input):
+	max_len_row = len(max(input, key=len))
+	padded = torch.zeros((len(input), max_len_row))
+	padded = torch.zeros((len(input), max_len_row))
+
+	for i in range(len(input)):
+		current_row_length = len(input[i])
+		padded[i, 0:current_row_length] = torch.tensor(input[i])
+	masked = padded > 0
+		
+	return padded, masked
+
+
