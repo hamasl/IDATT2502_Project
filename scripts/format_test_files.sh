d7d18541e4119042aa29d5965b4caeb3ad49e0a7
Diderik Kramer <diderikk@stud.ntnu.no>
2021-11-03 13:26:44 +0100

    Add parallellization

--- scripts/format_test_files.sh
+++ scripts/format_test_files.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
+
+format_files() {
+	mkdir ../formatted/"$1"
+	echo "$1"
+	for filename in $1/*.c; do
+		clang-format "$filename" > ../formatted/"$filename"
+		rm -f temp.c
+	done
+}
+
 rm -r ./formatted
 mkdir formatted
 cd data/
 for folder in CWE*; do
-	mkdir ../formatted/"$folder"
-	echo "$folder"
-	for filename in $folder/*.c; do
-		clang-format "$filename" > ../formatted/"$filename"
-		rm -f temp.c
-	done
+	format_files "$folder" &
 done
