dfc9408d0b9ef9644bf871d437dec840529f6d19
Diderik Kramer <diderikk@stud.ntnu.no>
2021-11-05 12:19:38 +0100

    Add doc

--- preprocessing/padding.py
+++ preprocessing/padding.py
@@ -1,6 +1,15 @@
 import torch
 
 def masking_padding(input):
+	"""
+	Input: A nested array containing tokenized functions
+	Finds the largest tokenized function array, and makes the length a multiple of four
+	Make each tokenized array the size of this number, and pads with 0, 
+	to create a padded version of the tokenized array
+	Creates a masked array to explain which values are padded
+
+	returns padded array and masked array
+	"""
 	max_len_row = len(max(input, key=len))
 	# Padded length must be multiple of 4
 	max_len_row += (max_len_row % 4)
