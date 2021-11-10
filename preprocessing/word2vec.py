39a7f7b93ce175d80c76758f72471c05a1daa627
Martin Hagen <martin@partins.no>
2021-11-10 13:53:53 +0100

    Added word2vec

--- preprocessing/word2vec.py
+++ preprocessing/word2vec.py
@@ -1,6 +1,8 @@
 # Implementation inspired by:
+import numpy as np
 import torch
 from torch.autograd import Variable
+from torch.autograd.grad_mode import F
 
 
 class Word2Vec:
@@ -9,7 +11,6 @@ class Word2Vec:
         self.vocab_size = vocab_size
         self.word_idx = word_idx
         self._input_layer = self._get_input_layer()
-        self._hidden_layer =
         self.embedding_dims = 5
 
     def _get_input_layer(self):
@@ -17,10 +18,28 @@ class Word2Vec:
         x[self.word_idx] = 1.0
         return x
 
-    def _get_hidden_layer(self):
-
+    def train(self, num_epochs, learning_rate, index_pairing):
         W1 = Variable(torch.randn(self.embedding_dims, self.vocab_size).float(), requires_grad=True)
-        return torch.matmul(W1, x)
+        W2 = Variable(torch.randn(self.vocab_size, self.embedding_dims).float(), requires_grad=True)
 
+        for epo in range(num_epochs):
+            loss_val = 0
+            for data, target in index_pairing:
+                x = Variable(self._input_layer(data)).float()
+                y_true = Variable(torch.from_numpy(np.array([target])).long())
 
-    def word2vec(corpus:[[str]], vocabulary:[str]):
+                z1 = torch.matmul(W1, x)
+                z2 = torch.matmul(W2, z1)
+
+                log_softmax = F.log_softmax(z2, dim=0)
+
+                loss = F.nll_loss(log_softmax.view(1, -1), y_true)
+                loss_val += loss.data[0]
+                loss.backward()
+                W1.data -= learning_rate * W1.grad.data
+                W2.data -= learning_rate * W2.grad.data
+
+                W1.grad.data.zero_()
+                W2.grad.data.zero_()
+            if epo % 10 == 0:
+                print(f'Loss at epo {epo}: {loss_val / len(index_pairing)}')
