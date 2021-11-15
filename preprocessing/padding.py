4a19b2436806d0e8a6b3ca3201f3ab2e70a1a156
Martin Hagen <martin@partins.no>
2021-11-15 13:00:44 +0100

    Added some documentation and refactored som code

--- preprocessing/padding.py
+++ preprocessing/padding.py
@@ -3,14 +3,12 @@ import torch
 
 def pad(inp, num_of_words):
     """
-	Input: A nested array containing tokenized functions
-	Finds the largest tokenized function array, and makes the length a multiple of four
-	Make each tokenized array the size of this number, and pads with 0, 
-	to create a padded version of the tokenized array
-	Creates a masked array to explain which values are padded
-
-	returns padded array and masked array
-	"""
+    Finds the largest tokenized function array.
+    Makes each tokenized array the size of this number, and pads with 0 to create a padded version of the tokenized array
+    :param inp: A nested array containing tokenized functions
+    :param num_of_words:
+    :return: padded array and masked array
+    """
     max_len_row = len(max(inp, key=len))
     # Padded length must be multiple of 4
     max_len_row += (max_len_row % 4)
--- test/test_generalizer.py
+++ test/test_generalizer.py
@@ -6,7 +6,7 @@ import preprocessing.keyword_dictionary as kd
 
 class NameSimplifierTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.dictionary = kd.get()
+        self.dictionary = kd.get_keywords()
 
     def test_handle_literals_str(self):
         inp = [["char", "*", "a", "=", '"hei hei"']]
@@ -36,20 +36,20 @@ class NameSimplifierTest(unittest.TestCase):
     def test_handle_functions_and_variables_multiple_variables_in_same_func(self):
         inp = [
             ["float", "a", "=", sc.BASE_CHAR, ";", "float", "b", "=", sc.BASE_CHAR, ";", "a", "=", sc.BASE_CHAR, ";"]]
-        expected = [["float", sc.GENERIC_VARIABLE_BASE_NAME, "=", sc.BASE_CHAR, ";", "float",
-                     sc.GENERIC_VARIABLE_BASE_NAME, "=", sc.BASE_CHAR, ";", sc.GENERIC_VARIABLE_BASE_NAME,
+        expected = [["float", sc.GENERIC_VARIABLE_NAME, "=", sc.BASE_CHAR, ";", "float",
+                     sc.GENERIC_VARIABLE_NAME, "=", sc.BASE_CHAR, ";", sc.GENERIC_VARIABLE_NAME,
                      "=", sc.BASE_CHAR, ";"]]
         self.assertEqual(ns.handle_functions_and_variables(inp, self.dictionary), expected)
 
     def test_handle_functions_and_variables_multiple_functions_ID_resets(self):
         inp = [["float", "a", "=", sc.BASE_CHAR, ";"], ["float", "b", "=", sc.BASE_CHAR, ";"]]
-        expected = [["float", sc.GENERIC_VARIABLE_BASE_NAME, "=", sc.BASE_CHAR, ";"], ["float",
-                                                                                             sc.GENERIC_VARIABLE_BASE_NAME,
+        expected = [["float", sc.GENERIC_VARIABLE_NAME, "=", sc.BASE_CHAR, ";"], ["float",
+                                                                                  sc.GENERIC_VARIABLE_NAME,
                                                                                              "=", sc.BASE_CHAR, ";"]]
         self.assertEqual(ns.handle_functions_and_variables(inp, self.dictionary), expected)
 
     def test_handle_functions_and_variables_func_name(self):
         inp = [["void", "parse", "(", "int", "a", ")", "{", "a", "+", "+", ";", "}"]]
-        expected = [["void", sc.GENERIC_FUNCTION_NAME, "(", "int", sc.GENERIC_VARIABLE_BASE_NAME, ")", "{",
-                     sc.GENERIC_VARIABLE_BASE_NAME, "+", "+", ";", "}"]]
+        expected = [["void", sc.GENERIC_FUNCTION_NAME, "(", "int", sc.GENERIC_VARIABLE_NAME, ")", "{",
+                     sc.GENERIC_VARIABLE_NAME, "+", "+", ";", "}"]]
         self.assertEqual(ns.handle_functions_and_variables(inp, self.dictionary), expected)
