fe8f8f3b89be0a308ddb4b69b586eee32230608a
Diderik Kramer <diderikk@stud.ntnu.no>
2021-11-16 12:44:17 +0100

    Fix epochs

--- model/__main__.py
+++ model/__main__.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     x = torch.load(os.path.join(dirname, "x.pt"))
     y = torch.load(os.path.join(dirname, "y.pt"))
     mod = cnn.ConvolutionalNeuralNetworkModel(int(torch.max(y).item()) + 1, x.shape[2], x.shape[3])
-    mod.train_model(x, y, 5, 1, verbose=True)
-    # mod.save_model_state()
+    mod.train_model(x, y, 50, 1, verbose=True)
+    mod.save_model_state()
--- model/cnn.py
+++ model/cnn.py
@@ -105,7 +105,7 @@ class ConvolutionalNeuralNetworkModel(nn.Module):
         :param x: The raw input of the x tensor
         :param y: The raw input of the y tensor
         :param batches: The number of bathes to be run for each epoch.
-        :param epochs: The number of epochs that the data should be trained for.
+        :param cross_validations: The number of cross validations that the data should be trained for.
         :param learning_rate: Decides how much the model "jump" for each time the data is being optimized. High learning rate may jump over minimas, while lower learning rate may get stuck a local minima.
         :param verbose: If the number of epochs completed should be printed to the console.
         :return: None
