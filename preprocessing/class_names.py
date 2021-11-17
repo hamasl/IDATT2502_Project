dd6ba8ab590f7598a840d1667be799cd15abd39b
Diderik Kramer <diderikk@stud.ntnu.no>
2021-11-17 15:26:23 +0100

    Add class names to file

---
+++ preprocessing/class_names.py
@@ -0,0 +1,5 @@
+class_names = ["None", "Uncontrolled Format String",
+               "Stack Based Buffer Overflow", "Memory Leak",
+               "Improper Initialization", "Plaintext Storage of Password",
+               "Divide by Zero", "Incorrect Check of Function Return Value",
+               "Use After Free", "Heap Based Buffer Overflow"]
