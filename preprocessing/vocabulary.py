db2632ad75d1da3aafce8d0180bdee94ddb68fb2
Martin Hagen <martin@partins.no>
2021-11-22 18:12:10 +0100

    Removed unused files

--- preprocessing/vocabulary.py
+++ preprocessing/vocabulary.py
@@ -12,12 +12,12 @@ def create_vocabulary(functions_list: [[]]):
     vocabulary = []
     for function in functions_list:
         for token in function:
-            if token not in vocabulary: vocabulary.append(token)
+            if token not in vocabulary:
+                vocabulary.append(token)
 
     word2idx = {w: idx for (idx, w) in enumerate(vocabulary)}
-    idx2word = {idx: w for (idx, w) in enumerate(vocabulary)}
 
-    return word2idx, idx2word
+    return word2idx
 
 
 def write_to_file(dictionary: dict):
