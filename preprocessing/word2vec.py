f8f89bf43bfa2d6382249c8e19323c0985c2fa3f
RokasBliu <r.bliudzius@gmail.com>
2021-11-23 15:36:14 +0100

    plot

--- preprocessing/word2vec.py
+++ preprocessing/word2vec.py
@@ -2,13 +2,16 @@
 import torch
 from torch.nn.functional import log_softmax, nll_loss
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
 
 
 class Word2Vec:
 
-    def __init__(self, vocab_size, word_idx, device=torch.device("cpu")):
+    def __init__(self, vocab_size, word2idx, idx2word, device=torch.device("cpu")):
         self.vocab_size = vocab_size
-        self.word_idx = word_idx
+        self.word2idx = word2idx
+        self.idx2word = idx2word
         self.embedding_dims = 5
         self._device = device
         self._W1 = torch.randn(self.embedding_dims, self.vocab_size, device=device, requires_grad=True,
@@ -36,8 +39,9 @@ class Word2Vec:
         :param word_idx2: The index of word2.
         :return: Int between 1 and -1, where 1 i equal.
         """
-        return torch.dot(self._W2[word_idx1], self._W2[word_idx2]) / (
-                    torch.norm(self._W2[word_idx1]) * torch.norm(self._W2[word_idx2]))
+        w1v = torch.matmul(self._W2, torch.matmul(self._W1, self.get_input_layer(word_idx1)))
+        w2v = torch.matmul(self._W2, torch.matmul(self._W1, self.get_input_layer(word_idx2)))
+        return torch.dot(w1v, w2v) / (torch.norm(w1v) * torch.norm(w2v))
 
     def train(self, num_epochs, learning_rate, index_pairing, verbose=False):
         """
@@ -66,3 +70,22 @@ class Word2Vec:
                 self._W2.grad.data.zero_()
             if verbose:
                 print(f'Loss at epo {epo}: {loss_val / len(index_pairing)}')
+
+    def get_closest_word(self, word, topn=5):
+        word_distance = []
+        i = self.word2idx[word]
+        for j in range(self.vocab_size):
+            if j != i:
+                word_distance.append((self.idx2word[j], self.similarity(j, i)))
+        word_distance.sort(key=lambda x: x[1], reverse=True)
+        return word_distance[:topn]
+
+    def plot(self, table, vocab):
+        pca = PCA(n_components=2)
+        pca.fit(table)
+        Xpca = pca.transform(table)
+        for i in range(len(vocab)):
+            plt.scatter(Xpca[i, 0], Xpca[i, 1])
+            plt.annotate(i, Xpca[i, 0], Xpca[i, 1], alpha=0.7)
+        plt.draw()
+        plt.show()
