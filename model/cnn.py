0adf096508c6018bb62cdf699051d8ac0c12b8e5
RokasBliu <r.bliudzius@gmail.com>
2021-11-24 10:37:35 +0100

    fix tests, remove bias

--- model/cnn.py
+++ model/cnn.py
@@ -4,14 +4,12 @@ import os
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-from torch import Tensor
 from torch.utils.data import random_split, TensorDataset
 
 
 class ConvolutionalNeuralNetworkModel(nn.Module):
     def __init__(self, num_of_classes: int, input_element_size: int, encoding_size_per_element: int,
-                 device=torch.device("cpu"), directory="state", class_names: [] = None,
-                 classification_bias: Tensor | None = None):
+                 device=torch.device("cpu"), directory="state", class_names: [] = None):
         """
         Creates a model object using the given parameters.
 
@@ -25,8 +23,6 @@ class ConvolutionalNeuralNetworkModel(nn.Module):
         self.encoding_size_per_element = encoding_size_per_element
         self.device = device
         self._dirname = os.path.join(os.path.dirname(__file__), directory)
-        # Does not matter if classifiaction_bias is none because cross entropy loss with None as weights behave as without weights.
-        self.classification_bias = classification_bias
         self.class_names = class_names
 
         # Model layers (includes initialized model variables):
@@ -69,10 +65,7 @@ class ConvolutionalNeuralNetworkModel(nn.Module):
         :param y: The y data.
         :return: Tensor containing the loss.
         """
-        return (nn.functional.cross_entropy(self.logits(x), y.to(self.device)).to(self.device)
-                if self.classification_bias is None else
-                nn.functional.cross_entropy(self.logits(x), y,
-                                            weight=self.classification_bias.to(self.device)).to(self.device))
+        return nn.functional.cross_entropy(self.logits(x), y.to(self.device)).to(self.device)
 
     # Accuracy
     def accuracy(self, x: torch.Tensor, y: torch.Tensor, batch_size: int) -> float:
